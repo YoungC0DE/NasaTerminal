088cf0dd86be59da7c69985e6c735e22dd0fa174
Rafael Anjos Santos <68437256+YoungC0DE@users.noreply.github.com>
2023-12-02 22:15:59 -0300

    [feature] create basic project

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,21 @@
 <template>
-  <RouterView />
+  <div class="c-body">
+    <RouterView />
+  </div>
 </template>
 
 <script setup>
 import { RouterView } from 'vue-router'
 </script>
 
-<style scoped>
+<style>
+* {
+  padding: 0px;
+  margin: 0px;
+  box-sizing: border-box;
+}
+
+.c-body {
+  background: url('@/assets/matrix_animation.gif');
+}
 </style>
