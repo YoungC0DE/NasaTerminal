74fab2291877a5c233cafb5b505ce2a11e92886b
Rafael Anjos Santos <68437256+YoungC0DE@users.noreply.github.com>
2023-12-05 23:21:47 -0300

    [feature] create basic project

--- src/core/config/endpoints.js
+++ src/core/config/endpoints.js
@@ -1,11 +1,11 @@
 import {
   APOD_COMMAND,
   ASTEROIDS_FEED_COMMAND,
-  ASTEROIDS_LOOKUP_COMMAND,
+  ASTEROIDS_LOOKUP_COMMAND
 } from '@/core/helpers/constants.js'
 
 export default {
-    [APOD_COMMAND]: 'https://api.nasa.gov/planetary/apod',
-    [ASTEROIDS_FEED_COMMAND]: 'https://api.nasa.gov/neo/rest/v1/feed',
-    [ASTEROIDS_LOOKUP_COMMAND]: 'https://api.nasa.gov/neo/rest/v1/neo',
+  [APOD_COMMAND]: 'https://api.nasa.gov/planetary/apod',
+  [ASTEROIDS_FEED_COMMAND]: 'https://api.nasa.gov/neo/rest/v1/feed',
+  [ASTEROIDS_LOOKUP_COMMAND]: 'https://api.nasa.gov/neo/rest/v1/neo'
 }
